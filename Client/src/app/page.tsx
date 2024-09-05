3972a1fad8b4b35db47a8319aeb77ad9666b919c
Mahid Ahmad <mahid797@hotmail.com>
2024-09-05 01:11:25 -0400

    Revert "Profile Tab Complete"
    This reverts commit 876a6ef70cae14a0586b8bcbb4c3225409095948.

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -2,5 +2,14 @@ import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
-	return <div>Home page</div>;
+	return (
+		<>
+			<div>Home page</div>;
+			<Link href="/profile" passHref>
+				<button type="button" className={styles['gradient-button']}>
+					Go to Profile
+				</button>
+			</Link>
+		</>
+	);
 }
