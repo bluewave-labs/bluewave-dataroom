57a67fe0cacded6eea182b24c2c5e344d98139d4
Mahid Ahmad <mahid797@hotmail.com>
2024-09-05 02:06:23 -0400

    Cleanup

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -1,15 +1,3 @@
-import Link from 'next/link';
-import styles from './page.module.css';
-
 export default function Home() {
-	return (
-		<>
-			<div>Home page</div>;
-			<Link href="/profile" passHref>
-				<button type="button" className={styles['gradient-button']}>
-					Go to Profile
-				</button>
-			</Link>
-		</>
-	);
+	return <div>Home page</div>;
 }
