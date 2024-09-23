1b260723af68742c3989bc3335b30dd106ddbf40
Neeraj Sunil <62223906+neerajsunil@users.noreply.github.com>
2024-09-23 03:10:25 -0600

    Removed back button

--- Client/src/app/contacts/page.tsx
+++ Client/src/app/contacts/page.tsx
@@ -7,18 +7,6 @@ export default function ContactsPage() {
 	return (
 		<Stack spacing={4} maxWidth="lg" sx={{ pb: '15rem', pt: '4rem' }}>
 			<Box>
-				<Link href="/" passHref>
-					<Button
-						color="secondary"
-						variant="outlined"
-						size="small"
-						sx={{ paddingX: '1.2rem', paddingY: '0.1rem' }}
-						startIcon={<ArrowBackIcon />}>
-						Back
-					</Button>
-				</Link>
-			</Box>
-			<Box>
 				<Typography
 					sx={{
 						fontWeight: '600',
