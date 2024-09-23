6dd82395ed93de99ff2a3b606c7c7fb36b9171ed
Neeraj Sunil <62223906+neerajsunil@users.noreply.github.com>
2024-09-23 04:02:56 -0600

    Fixed folder consistency and added theme provider with sidebar

--- Client/src/app/layout.tsx
+++ Client/src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-
-import Sidebar from "../Components/Sidebar/Sidebar";
 import { Container } from "@mui/material";
+import Sidebar from "@/components/sidebar/Sidebar";
+import CssBaseline from "@mui/material/CssBaseline";
+import globalTheme from "@/utils/theme/globalTheme";
+import { ThemeProvider } from "@mui/material/styles";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,28 +13,30 @@ export const metadata: Metadata = {
   description: "Share documents safely with your team and customers",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <ThemeProvider theme={globalTheme}>
         <Container
-          maxWidth={"lg"}
           sx={{
             display: "flex",
             backgroundColor: "white",
             height: "100vh",
-            pt: 4,
-            pb: 3,
+            paddingTop: 4,
+            paddingBottom: 3,
             gap: 4,
           }}
         >
           <Sidebar />
-          <>{children}</>
+          <CssBaseline />
+          {children}
         </Container>
+        </ThemeProvider>
       </body>
     </html>
   );
