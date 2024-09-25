ea26d351b4a88142d17fdb974d437314916ce553
Neeraj Sunil <62223906+neerajsunil@users.noreply.github.com>
2024-09-25 10:08:14 -0600

    Added authentication

---
+++ Server/utils/utils.js
@@ -0,0 +1,33 @@
+/**
+ * Converts a request body parameter to a boolean.
+ * @param {string | boolean} value
+ * @returns {boolean}
+ */
+const ParseBoolean = (value) => {
+    if (value === true || value === "true") {
+      return true;
+    } else if (
+      value === false ||
+      value === "false" ||
+      value === null ||
+      value === undefined
+    ) {
+      return false;
+    }
+  };
+  
+  const getTokenFromHeaders = (headers) => {
+    const authorizationHeader = headers.authorization;
+    if (!authorizationHeader) throw new Error("No auth headers");
+  
+    const parts = authorizationHeader.split(" ");
+    if (parts.length !== 2 || parts[0] !== "Bearer")
+      throw new Error("Invalid auth headers");
+  
+    return parts[1];
+  };
+  
+  module.exports = {
+    ParseBoolean,
+    getTokenFromHeaders,
+  };
