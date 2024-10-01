de1d9d3ceb869e94219e74a5c1693643abd9ee16
Sepideh Shahbazi <sepideh.shahbazi@gmail.com>
2024-09-30 20:13:17 -0700

    Refactored DeleteConfirmationModal

--- Client/src/Components/DeleteConfirmationModal/DeleteConfirmationModal.tsx
+++ Client/src/Components/DeleteConfirmationModal/DeleteConfirmationModal.tsx
@@ -1,97 +1,94 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
-import { FC } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  DialogContentText,
+} from "@mui/material";
 
-type DeleteConfirmationModalProps = {
+interface DeleteConfirmationModalProps {
   itemToDelete: string;
-  handleDelete: any;
-  setShowDeleteModal: any;
-};
+  paragraphText: string;
+  buttonText: string;
+  handleDelete: () => void;
+  setShowDeleteModal: React.Dispatch<React.SetStateAction<boolean>>;
+  showDeleteModal: boolean;
+}
 
-const DeleteConfirmationModal: FC<DeleteConfirmationModalProps> = ({
+export default function DeleteConfirmationModal({
   itemToDelete,
+  paragraphText,
+  buttonText,
   handleDelete,
   setShowDeleteModal,
-}) => {
+  showDeleteModal,
+}: DeleteConfirmationModalProps) {
   //close the DeleteConfirmationModal component
-  const handleCancle = () => {
+  const handleClose = () => {
     setShowDeleteModal(false);
   };
 
   return (
     <>
-      <Box
+      <Dialog
         sx={{
-          boxShadow: "0px 20px 24px -4px #10182814",
-          border: "1px solid #e5e5e540",
-          width: "439px",
-          height: "186",
-          borderRadius: "4px",
-          p: "32px",
-          zIndex: 1,
+          "& .MuiDialog-paper": {
+            // Modify the style of the internal div
+            width: 439,
+            p: 15,
+          },
         }}
+        open={showDeleteModal}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
       >
-        <Stack direction={"column"} spacing={"30px"}>
-          <Box>
-            <Typography
-              variant="body1"
-              sx={{
-                fontSize: "13px",
-                fontWeight: 600,
-                color: "#344054",
-                mb: "10px",
-              }}
-            >
-              Really delete this {itemToDelete}?
-            </Typography>
-            <Typography
-              variant="body2"
-              sx={{
-                fontSize: "13px",
-                lineHeight: "20px",
-                color: "#344054",
-                textAlign: "justify",
-              }}
-            >
-              When you delete this {itemToDelete}, all the links associated with
-              the {`${itemToDelete} `} will also be removed. Note that this is a
-              non-reversible action.
-            </Typography>
-          </Box>
-          <Box sx={{ direction: "rtl" }}>
-            <Button
-              variant="contained"
-              sx={{
-                width: "119px",
-                height: "34px",
-                padding: "8px 12px 8px 12px",
-                borderRadius: "4px",
-                backgroundColor: "#DB504A",
-                ml: "32px",
-                fontSize: "13px",
-                textTransform: "none",
-              }}
-              onClick={handleDelete}
-            >
-              Delete {itemToDelete}
-            </Button>
-            <Button
-              sx={{
-                width: "43px",
-                height: "24px",
-                fontSize: "13px",
-                color: "#344054",
-                display: "inline-flex",
-                textTransform: "none",
-              }}
-              onClick={handleCancle}
-            >
-              Cancel
-            </Button>
-          </Box>
-        </Stack>
-      </Box>
+        <DialogTitle
+          sx={{
+            fontSize: "13px",
+            color: "#344054",
+            p: 0,
+            mb: 5,
+          }}
+        >
+          Really delete this {itemToDelete}?
+        </DialogTitle>
+        <DialogContent sx={{ p: 0 }}>
+          <DialogContentText
+            id="alert-dialog-slide-description"
+            sx={{
+              fontSize: "13px",
+              color: "#344054",
+              textAlign: "justify",
+            }}
+          >
+            {paragraphText}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions sx={{ p: 0, mt: 15 }}>
+          <Button
+            sx={{
+              color: "#344054",
+              mr: 15,
+            }}
+            onClick={handleClose}
+          >
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              py: 3,
+              px: 12,
+              backgroundColor: "#DB504A",
+            }}
+            onClick={handleDelete}
+          >
+            {buttonText}
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
-};
-
-export default DeleteConfirmationModal;
+}
