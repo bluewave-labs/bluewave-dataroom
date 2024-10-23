e0eeb4c6ab94b82fc7e46b9d66f0f1a5b07745b3
sajan <sajanghuman18@gmail.com>
2024-10-23 09:47:05 -0500

    Resolve requested changes for PR
    Rebase PR for a clean history

--- Client/src/app/api/auth/[...nextauth]/route.ts
+++ Client/src/app/api/auth/[...nextauth]/route.ts
@@ -64,15 +64,6 @@ export const authOptions: NextAuthOptions = {
 					return null;
 				}
 
-				//// For some reason, supabase does not store bigint values. Therefore for every file where id is fetched or stored, we have converted it to string first
-				//console.log('User authorized:', {
-				//	id: user.id.toString(), // Convert bigint to string
-				//	userId: user.user_id,
-				//	email: user.email,
-				//	name: user.name,
-				//	role: user.role,
-				//});
-
 				return {
 					id: user.id.toString(), // Convert bigint to string
 					userId: user.user_id,
