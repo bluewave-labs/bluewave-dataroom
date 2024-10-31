b4fb553f665ed5cded12628d9c8120c112ee7e92
Sepideh Shahbazi <sepideh.shahbazi@gmail.com>
2024-10-31 15:51:22 -0700

    bug/Fixing pagination icons

---
+++ Client/public/assets/icons/pagination/ArrowForwardIcon.tsx
@@ -0,0 +1,14 @@
+const ArrowForwardIcon = () => {
+	return (
+		<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path
+				d="M1.16699 6.99984H12.8337M12.8337 6.99984L7.00033 1.1665M12.8337 6.99984L7.00033 12.8332"
+				stroke="#667085"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			/>
+		</svg>
+	);
+};
+
+export default ArrowForwardIcon;
