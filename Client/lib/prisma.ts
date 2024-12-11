d0f9ef45e71706f05d690237d8b22d953571dc26
Mahid Ahmad <31464238+mahid797@users.noreply.github.com>
2024-12-11 09:58:27 -0500

    Merge fix 2 - Continued (#123)
    * Fix issues from #92 and #93 (#99)
    
    * Fix issues from #92 and #93
    
    * Fix auto signIn after password reset
    
    * Implement requested changes
    
    * Fixed Forget-Passoword Flow
    
    ---------
    
    Co-authored-by: Mahid Ahmad <mahid797@hotmail.com>
    
    * Documents - Upload V1.0
    
    * Documents - List V1.0
    
    * Merge branch 'master' of https://github.com/bluewave-labs/bluewave-dataroom
    
    * Document V2.0
    
    * Toast Refactored
    
    ---------
    
    Co-authored-by: SajanGhuman <121523783+SajanGhuman@users.noreply.github.com>

--- Client/lib/prisma.ts
+++ Client/lib/prisma.ts
@@ -1,18 +1,13 @@
-// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-if (process.env.NODE_ENV === 'production') {
-	// In production, create a single instance of PrismaClient
-	prisma = new PrismaClient();
-} else {
-	// In development, use a global variable so that PrismaClient is not
-	// created multiple times during hot reloading
-	if (!global.prisma) {
-		global.prisma = new PrismaClient();
-	}
-	prisma = global.prisma;
-}
+const prisma =
+	globalForPrisma.prisma ||
+	new PrismaClient({
+		log: ['query', 'info', 'warn', 'error'],
+	});
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
