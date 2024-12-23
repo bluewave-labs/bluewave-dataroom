aaa70141abfd61be1e54bfcebff5845bc7d96f96
Parwat Kunwar <parwatkunwar08@gmail.com>
2024-12-23 00:27:42 -0400

    backend(linkService): Add class LinkService for services methods

---
+++ Client/src/services/linkService.ts
@@ -0,0 +1,64 @@
+import { v4 as uuidv4 } from 'uuid';
+import prisma from '@lib/prisma';
+import { SupabaseProvider } from '@/providers/storage/supabase/supabaseProvider';
+
+
+export type LinkMetaDataType = {
+  linkId: string;
+  linkName: string;
+  isPublic: boolean;
+  emailRequired: boolean;
+  passwordRequired: boolean;
+  linkPassword?: string;
+  canExpire: boolean;
+  expirationDateTimestamp?: number;
+  createdBy: String;
+  userId: string;
+  documentId: string;
+};
+
+export default class LinkService {
+
+  static fieldsToPick = ['userId',
+    'documentId',
+    'fileName',
+    'customName',
+    'linkUrl',
+    'isPublic',
+    'emailRequired',
+    'passwordRequired',
+    'linkPassword',
+    'expirationTime',
+    'createdBy'];
+
+  static generateLinkUrl(host: string): string {
+    const uniqueId = uuidv4();
+
+    return `${host}/${uniqueId}`;
+  }
+
+  static async deleteLink(linkId: string): Promise<void> {
+    return;
+  }
+
+  static async getFileFromLink(linkId: string): Promise<string> {
+    const bucketName = 'documents';
+    let link = await prisma.link.findUnique({
+      where: {
+        linkId
+      },
+      include: {
+        Document: true
+      }
+    });
+
+    if (!link || !link.Document) {
+      throw new Error('Link not found');
+    }
+
+    const supabaseProvider = new SupabaseProvider();
+    const signedUrl = await supabaseProvider.generateSignedUrl(bucketName, link.Document.filePath);
+
+    return signedUrl;
+  }
+}
