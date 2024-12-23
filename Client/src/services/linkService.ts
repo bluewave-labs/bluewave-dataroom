4893a589244d535cc55c9f648f16db34647becc8
Parwat Kunwar <parwatkunwar08@gmail.com>
2024-12-23 00:27:42 -0400

    backend(linkService): Add class LinkService for services methods

--- Client/src/services/linkService.ts
+++ Client/src/services/linkService.ts
@@ -3,53 +3,52 @@ import prisma from '@lib/prisma';
 import { SupabaseProvider } from '@/providers/storage/supabase/supabaseProvider';
 
 export default class LinkService {
+	static generateLinkDetails(): { linkUrl: string; linkId: string } {
+		const uniqueId = uuidv4();
+		const HOST = process.env.HOST || 'http://localhost:3000';
 
-  static generateLinkDetails(): { linkUrl: string, linkId: string; } {
-    const uniqueId = uuidv4();
-    const HOST = process.env.HOST || 'http://localhost:3000';
+		return {
+			linkId: uniqueId,
+			linkUrl: `${HOST}/${uniqueId}`,
+		};
+	}
 
-    return {
-      linkId: uniqueId,
-      linkUrl: `${HOST}/${uniqueId}`
-    };
-  }
+	static async deleteLink(linkId: string): Promise<void> {
+		return;
+	}
 
-  static async deleteLink(linkId: string): Promise<void> {
-    return;
-  }
+	static async getLink(linkId: string) {
+		return prisma.link.findUnique({
+			where: { linkId },
+		});
+	}
 
-  static async getLink(linkId: string) {
-    return prisma.link.findUnique({
-      where: { linkId },
-    });
-  }
+	static async getFileFromLink(linkId: string): Promise<string> {
+		const bucketName = 'documents';
+		let link = await prisma.link.findUnique({
+			where: {
+				linkId,
+			},
+			include: {
+				Document: true,
+			},
+		});
 
-  static async getFileFromLink(linkId: string): Promise<string> {
-    const bucketName = 'documents';
-    let link = await prisma.link.findUnique({
-      where: {
-        linkId
-      },
-      include: {
-        Document: true
-      }
-    });
+		if (!link || !link.Document) {
+			throw [404, 'Link not found'];
+		}
 
-    if (!link || !link.Document) {
-      throw [404, "Link not found"];
-    }
+		if (!link.isPublic) {
+			throw [403, 'Link is not public'];
+		}
 
-    if (!link.isPublic) {
-      throw [403, "Link is not public"];
-    }
+		if (link.expirationTime && new Date(link.expirationTime) < new Date()) {
+			throw [410, 'Link has expired'];
+		}
 
-    if (link.expirationTime && new Date(link.expirationTime) < new Date()) {
-      throw [410, "Link has expired"];
-    }
+		const supabaseProvider = new SupabaseProvider();
+		const signedUrl = await supabaseProvider.generateSignedUrl(bucketName, link.Document.filePath);
 
-    const supabaseProvider = new SupabaseProvider();
-    const signedUrl = await supabaseProvider.generateSignedUrl(bucketName, link.Document.filePath);
-
-    return signedUrl;
-  }
+		return signedUrl;
+	}
 }
