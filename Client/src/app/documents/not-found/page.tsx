e0753e691476cbad20b3c5fe6624cd60901a17a7
Mahid Ahmad <mahid797@hotmail.com>
2025-01-13 09:48:16 -0500

    Refactor document details page structure by removing obsolete components and adding new dynamic document view and loading components

--- Client/src/app/documents/not-found/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function Home() {
-    return (<div>Document not found</div>);
-  }
-  
--- Client/src/app/documents/view-document/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function Home() {
-    return (<div>View Document</div>);
-  }
-  
